a6aacf66c6df596b854a3d53f2b9e3235432a10e
lichomonjelat <60107296+lichomonjelat@users.noreply.github.com>
2020-10-15 20:28:38 -0300

    js funcionando. modificaciones generales

--- js/app.js
+++ js/app.js
@@ -1,6 +1,3 @@
-alert("Bienvenido al cotizador online");
-var servicio =prompt("Ingrese un servicio a cotizar");
-
 var agroquimico =['Glifosato', 'D4D', 'Agrodyne'];
 
 var seleccion =["Siembra", "Fumigaciones", "Traslados", "Funguicidas"];
@@ -9,52 +6,90 @@ var costoSiembra = (7);
 var costoFumigacion =(9);
 var costoKilometro =(4.6);
 
-function precioSiembra(hectareas, costoSiembra){
-    var preciototal= (hectareas * costoSiembra)
-    alert("El costo es" + preciototal +"dolares" )
+var intro = document.getElementById("intro");
+var select = document.getElementById("select");
+var result = document.getElementById("result");
+var preview = document.getElementById("preview");
 
+var inputNombre = [];    
+function getName(){
+    Nombre = document.getElementById("tunombre").value;
+    inputNombre = Nombre;
+localStorage.setItem("Nombre",inputNombre)    
+
+};
+var inputApellido = [];    
+function getLastname(){
+    Apellido = document.getElementById("tuapellido").value;
+    inputApellido = Apellido;
+localStorage.setItem("Apellido", inputApellido);    
+
+};
+var inputServicios = [];    
+function getService(){
+    Servicio = document.getElementById("servicios").value;
+    inputServicios = Servicio;
+localStorage.setItem("Servicio", inputServicios);
+};
+var inputHectareas = [];    
+function getHectareas(){
+    Hectareas = document.getElementById("hectareas").value;
+    inputHectareas = Hectareas;
+localStorage.setItem("areaTrabajo", inputHectareas);
+};
+var inputKilometros = [];    
+function getKilometros(){
+    Kilometros = document.getElementById("kilometros").value;
+    inputKilometros = Kilometros;
+localStorage.setItem("distanciaKM", inputKilometros);
+};
+
+function precioSiembra(hectareas, costoSiembra){
+    let preciototal= (hectareas * costoSiembra)
+    result.innerHTML = "El costo será de" + " " +preciototal+ " "+ "dolares";
 }
 function precioFumigacion(hectareas, costoFumigacion){
-    var preciototal= (hectareas * costoFumigacion)
-    alert("El costo es" + preciototal +"Dolares" )
+    let preciototal= (hectareas * costoFumigacion)
+    result.innerHTML = "El costo será de" + " " +preciototal+ " "+ "dolares";
 
 }
 function precioKilometro(kilometros, costoKilometro){
-    var preciototal= (kilometros * costoKilometro)
-    alert("El costo es" + preciototal +"Dolares" )
+    let preciototal= (kilometros * costoKilometro)
+    result.innerHTML = "El costo será de" + " " +preciototal+ " "+ "dolares";
 
 }
-
-if (servicio==("siembra")){
+switch(localStorage.Servicio){
     
-    alert("Usted Seleccionó" + seleccion [0]);
-    var hectareas =prompt("Ingrese las hectareas a trabajar");
-    alert("Tenes" + hectareas + "Hectareas para sembrar");
-    precioSiembra(hectareas, costoSiembra)
-
-}
-else if (servicio==("fumigaciones")){
-
-    alert("Usted Seleccionó" + seleccion [1]);
-    var hectareas =prompt("Ingrese las hectareas a trabajar");
-    alert("Tenes" + hectareas + "Hectareas para fumigar");
-    precioFumigacion(hectareas, costoFumigacion)
-
-}
-else if (servicio==("traslados")){
-
-    alert("Usted Seleccionó" + seleccion [2]);
-    var kilometros =prompt("Cuantos kilometros tenes que recorrer?");
-    alert("Seran" + kilometros + "Kilometros para recorrer");
-    precioKilometro(kilometros, costoKilometro)
-
-}
-else {
-    alert("ingrese una respuesta valida")
-}
     
+    case "siembra":
+        intro.innerHTML = "Bienvenido" + " " + localStorage.getItem("Nombre"); 
+        select.innerHTML ="Usted Seleccionó"+ " " + seleccion [0];
+        var hectareas = localStorage.getItem("areaTrabajo");
+        preview.innerHTML = "Tenes" +" " + hectareas +" "+"hectareas" +" "+"para sembrar";
+        precioSiembra(localStorage.areaTrabajo, costoSiembra)
+        localStorage.clear
+        break;
+    case "pulverizaciones":
+        intro.innerHTML = "Bienvenido" + " " + localStorage.getItem("Nombre"); 
+        select.innerHTML ="Usted Seleccionó"+ " " + seleccion [1];
+        var hectareas = localStorage.getItem("areaTrabajo");
+        preview.innerHTML = "Tenes" +" " + hectareas +" "+"hectareas" +" "+"para fumigar";
+        precioFumigacion(hectareas, costoFumigacion);
+        localStorage.clear
+        break;
+    case "traslados":
+        intro.innerHTML = "Bienvenido" + " " + localStorage.getItem("Nombre");
+        select.innerHTML ="Usted Seleccionó"+ " " + seleccion [2]; 
+        var kilometros =localStorage.getItem("distanciaKM");
+        preview.innerHTML = "Tenes" +" " + kilometros  +" "+"kilometros para volar";
+        precioKilometro(kilometros, costoKilometro)   
+        localStorage.clear
+        break;
+    default:
+        alert("ingrese una respuesta valida")
+}
 
- 
 
 
-    
+
+
