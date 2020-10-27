d32bb7eb1f05ba9f261548d1321d151a8524f66e
lichomonjelat <60107296+lichomonjelat@users.noreply.github.com>
2020-10-27 19:54:03 -0300

    working js bg

--- js/app.js
+++ js/app.js
@@ -6,40 +6,36 @@ var costoSiembra = (7);
 var costoFumigacion =(9);
 var costoKilometro =(4.6);
 
-var intro = document.getElementById("intro");
-var select = document.getElementById("select");
-var result = document.getElementById("result");
-var preview = document.getElementById("preview");
-
 var inputNombre = [];    
 function getName(){
-    Nombre = document.getElementById("tunombre").value;
+    Nombre = $("#tunombre").val();
     inputNombre = Nombre;
 localStorage.setItem("Nombre",inputNombre)    
 
 };
 var inputApellido = [];    
+
 function getLastname(){
-    Apellido = document.getElementById("tuapellido").value;
+    Apellido = $("#tuapellido").val();
     inputApellido = Apellido;
 localStorage.setItem("Apellido", inputApellido);    
 
 };
 var inputServicios = [];    
 function getService(){
-    Servicio = document.getElementById("servicios").value;
+    Servicio = $("#servicios").val();
     inputServicios = Servicio;
 localStorage.setItem("Servicio", inputServicios);
 };
 var inputHectareas = [];    
 function getHectareas(){
-    Hectareas = document.getElementById("hectareas").value;
+    Hectareas = $("#hectareas").val();
     inputHectareas = Hectareas;
 localStorage.setItem("areaTrabajo", inputHectareas);
 };
 var inputKilometros = [];    
 function getKilometros(){
-    Kilometros = document.getElementById("kilometros").value;
+    Kilometros = $("#kilometros").val();
     inputKilometros = Kilometros;
 localStorage.setItem("distanciaKM", inputKilometros);
 };
@@ -58,30 +54,41 @@ function precioKilometro(kilometros, costoKilometro){
     result.innerHTML = "El costo será de" + " " +preciototal+ " "+ "dolares";
 
 }
+function getData(){
+    getName(),getLastname(),getService(),getHectareas(),getKilometros();
+}
+/*$("#submitter").click({
+    getName(),getLastname(),getService(),getHectareas(),getKilometros();
+});*/
+
+
+
+
+
 switch(localStorage.Servicio){
     
     
     case "siembra":
-        intro.innerHTML = "Bienvenido" + " " + localStorage.getItem("Nombre"); 
-        select.innerHTML ="Usted Seleccionó"+ " " + seleccion [0];
+        $("#intro").html("Bienvenido" + " " + localStorage.getItem("Nombre"))
+        $("#select").html("Usted Seleccionó"+ " " + seleccion [0])
         var hectareas = localStorage.getItem("areaTrabajo");
-        preview.innerHTML = "Tenes" +" " + hectareas +" "+"hectareas" +" "+"para sembrar";
+        $("#preview").html("Tenes" +" " + hectareas +" "+"hectareas" +" "+"para sembrar")
         precioSiembra(localStorage.areaTrabajo, costoSiembra)
         localStorage.clear
         break;
     case "pulverizaciones":
-        intro.innerHTML = "Bienvenido" + " " + localStorage.getItem("Nombre"); 
-        select.innerHTML ="Usted Seleccionó"+ " " + seleccion [1];
+        $("#intro").html("Bienvenido" + " " + localStorage.getItem("Nombre"))
+        $("#select").html("Usted Seleccionó"+ " " + seleccion [1])
         var hectareas = localStorage.getItem("areaTrabajo");
-        preview.innerHTML = "Tenes" +" " + hectareas +" "+"hectareas" +" "+"para fumigar";
+        $("#preview").html("Tenes" +" " + hectareas +" "+"hectareas" +" "+"para fumigar")
         precioFumigacion(hectareas, costoFumigacion);
         localStorage.clear
         break;
     case "traslados":
-        intro.innerHTML = "Bienvenido" + " " + localStorage.getItem("Nombre");
-        select.innerHTML ="Usted Seleccionó"+ " " + seleccion [2]; 
+        $("#intro").html("Bienvenido" + " " + localStorage.getItem("Nombre"))
+        $("#select").html("Usted Seleccionó"+ " " + seleccion [2]) 
         var kilometros =localStorage.getItem("distanciaKM");
-        preview.innerHTML = "Tenes" +" " + kilometros  +" "+"kilometros para volar";
+        $("#preview").html("Tenes" +" " + kilometros  +" "+"kilometros para volar") 
         precioKilometro(kilometros, costoKilometro)   
         localStorage.clear
         break;
@@ -89,6 +96,11 @@ switch(localStorage.Servicio){
         alert("ingrese una respuesta valida")
 }
 
+ 
+particlesJS.load('particles-js', '../js/particles.json',
+function(){
+    console.log('particles.json loaded...')
+})
 
 
 
